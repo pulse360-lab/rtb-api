9f83038aa2c46aa1fe14a5e2c281a92cfed3fdb0
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-02-08 00:54:18 +0000

    add method get by stop and ajust on redis client

--- src/routes.js
+++ src/routes.js
@@ -8,4 +8,6 @@ module.exports = (app) => {
     app.get(`/${prefix}/locale`, locale.get),
     app.get(`/${prefix}/realtime/info`, realTime.getInfo)
     app.get(`/${prefix}/realtime/bystop`, realTime.getByStop)
+    app.get(`/${prefix}/realtime/stopnear`, realTime.getStopsNear)
+    
 };
