421ae2233a56be7c4f592a83365fe7769e90f83a
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-02-08 01:01:58 +0000

    code refactoring

--- src/helper/api.js
+++ src/helper/api.js
@@ -1,13 +1,11 @@
 const apiFactory = require('../api-clients/api-factory');
 
-const exec = async (location, client) =>{
+module.exports = async (location, callback) =>{
     try {
        let api = apiFactory.getInstance(location.city);
-       let result = await client(api);
+       let result = await callback(api);
        return result;
     } catch(error){
         return {msgError: 'Service unavailable for your current location'};
     }
-}
-
-module.exports = { exec };
+};
