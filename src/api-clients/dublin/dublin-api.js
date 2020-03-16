d313dc5ca41e53b14784b9abfb33b68d30b9db37
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-03-16 01:22:27 +0000

    language resource implementation for Dublin API

--- src/api-clients/dublin/dublin-api.js
+++ src/api-clients/dublin/dublin-api.js
@@ -1,32 +1,39 @@
 const apiBase = require('../api-client-base');
 const routes = require('./route-list.json');
+const config = require('../../../config.json');
+const language = require('./languages/language');
 
 class dublinApi extends apiBase{
-    constructor(){
-        super("Dublin");
-        super.setBaseUri("https://data.smartdublin.ie/cgi-bin/rtpi/");
+    constructor(language){
+        super(config.apis.dublin.key);
+        super.setBaseUri(config.apis.dublin.url);
+    }
+
+    
+    getResult(map, result){
+        return result && result.errorcode === "0" 
+        ? map.mapObjectResult(result)
+        : {error: {message: this.resource.error_message}};
     }
 
     async getStopInformation(param){
-        let result = await this.httpClient.get(`${ this.baseUrl }busstopinformation?stopid=${param.stopNumber}&format=json`);
-
-        return result.errorcode === "1" 
-                                ? {error: {message: result.errormessage}} 
-                                : require('./responses/stop-information').mapObjectResult(result);
+        this.resource = language.getResource(param.language);
+        let response = await this.httpClient.get(`${ this.baseUrl }busstopinformation?stopid=${param.stopNumber}&format=json`);
+        var result =  this.getResult(require('./responses/stop-information'), response);
+        return result;
     }
 
     async getRealTimeInformation(param){
-        let result = await this.httpClient.get(`${ this.baseUrl }realtimebusinformation?stopid=${param.stopNumber}&routeid=${param.routeId}&operator=${param.operator}`);
-
-        return result.errorcode === "1" 
-                                ? {error: {message: result.errormessage}} 
-                                : require('./responses/realtime-information').mapObjectResult(result);
+        this.resource = language.getResource(param.language);
+        let response = await this.httpClient.get(`${ this.baseUrl }realtimebusinformation?stopid=${param.stopNumber}&routeid=${param.routeId}&operator=${param.operator}`);
+        var result =  this.getResult(require('./responses/realtime-information'), response);
+        return result;
     }
 
     getStopsNearMe(locationRange){
         var stops = [];
 
-        require('./route-list.json').forEach(f => {
+        routes.forEach(f => {
             if(f.latitude >= locationRange.minLat && f.latitude <= locationRange.maxLat && f.longitude >= locationRange.minLon && f.longitude <= locationRange.maxLon)
                 stops.push({stopId: f.stopid, name: f.fullname});
         });
